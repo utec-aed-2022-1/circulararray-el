9936607ca7561251c2bb0b3edba8e90b3eaceb25
Manuel Alvarez <alvarezmanuel076@gmail.com>
2022-03-28 17:29:38 -0500

    Clase Circuilar Array, Stack y Queue implementadas. Métodos de Circular Array implementados. Métodos de Stack y Queue heredados desde Circular Array.

--- circulararray.h
+++ circulararray.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 template <class T>
@@ -8,7 +9,7 @@ private:
     T *array;
     int capacity;
     int back, front;
-    
+
 public:
     CircularArray();
     CircularArray(int _capacity);
@@ -26,7 +27,7 @@ public:
     void sort();
     bool is_sorted();
     void reverse();
-    string to_string(string sep=" ");
+    string to_string(string sep = " ");
 
 private:
     int next(int);
@@ -68,8 +69,128 @@ int CircularArray<T>::next(int index)
 template <class T>
 string CircularArray<T>::to_string(string sep)
 {
-    string result = ""; 
+    string result = "";
     for (int i = 0; i < size(); i++)
-        result += std::to_string((*this)[i]) + sep;
-    return result;    
+        result += std::to_string(array[i]) + sep;
+    return result;
+}
+
+template <class T>
+void CircularArray<T>::push_front(T data)
+{
+    if (front < 0)
+        front = back = 0;
+    for (int i = back; i >= 0; i--)
+    {
+        array[i + 1] = array[i];
+    }
+    ++back;
+    array[front] = data;
+}
+
+template <class T>
+void CircularArray<T>::push_back(T data)
+{
+    if (back < 0)
+    {
+        front = back = 0;
+        array[back] = data;
+    }
+    else
+        array[++back] = data;
+}
+
+template <class T>
+void CircularArray<T>::insert(T data, int pos)
+{
+}
+
+template <class T>
+T CircularArray<T>::pop_front()
+{
+    for (int i = 1; i < back; i++)
+    {
+        array[i - 1] = array[i];
+    }
+    --back;
+    return array[front];
+}
+
+template <class T>
+T CircularArray<T>::pop_back()
+{
+    T *temp = new T;
+    *temp = array[back];
+    if (back == 0)
+        back = capacity;
+    else
+        back--;
+    return *temp;
+}
+
+template <class T>
+bool CircularArray<T>::is_full()
+{
+    return (back == capacity - 1) ? true : false;
+}
+
+template <class T>
+bool CircularArray<T>::is_empty()
+{
+    return (front < 0) ? true : false;
+}
+
+template <class T>
+int CircularArray<T>::size()
+{
+    return front > back ? (capacity - front + back + 1) : (back - front + 1);
+}
+
+template <class T>
+void CircularArray<T>::clear()
+{
+}
+
+template <class T>
+T &CircularArray<T>::operator[](int)
+{
+}
+
+template <class T>
+void CircularArray<T>::sort()
+{
+    for (int i = 0; i < size()-1; i++) {   
+     
+
+    for (int j = 0; j < size()-i-1; j++){
+        if (array[j] > array[j+1]){
+            int temp = array[j];
+            array[j] = array[j+1];
+            array[j+1] = temp;
+      }
+      }
+      }
+}
+
+template <class T>
+bool CircularArray<T>::is_sorted()
+{
+    for (int i = 1; i < back; i++)
+    {
+        if (array[i - 1] > array[i])
+            return false;
+    }
+
+    return true;
+}
+
+template <class T>
+void CircularArray<T>::reverse()
+{
+    for (int low = 0, high = size() - 1; low < high; low++, high--) {
+        T temp;
+        temp = array[low];
+        array[low] = array[high];
+        array[high] = temp;
+    }
 }
--- queue.h
+++ queue.h
@@ -1,6 +1,38 @@
 #include "circulararray.h"
 
 template <typename T>
-class QueueArray : public CircularArray<T> { 
-    
+class QueueArray : public CircularArray<T> {
+private:
+    int *array;
+    int capacity;
+    int size;
+public:
+    Queue(){
+        T *array = nullptr;
+        capacity = size = 0;
+    }
+    Queue(int cap){
+        capacity = cap;
+        array = new T[capacity];
+        size = 0;
+    }
+    void enqueue(T data){
+        push_back(data);
+    }
+
+    void dequeue(){
+        pop_front();
+    }
+
+    void display(){
+        for(int i = 0; i<size; i++){
+            cout << array[i] << " ";
+        }
+        cout << endl;
+    }
+
+    int size(){
+        size();
+    }
+
 };
